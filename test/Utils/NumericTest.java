538e0c9f4c6ba3722c33276f58982d774a0d7c46
Max Bauer <mbauer7532@gmail.com>
2013-10-19 19:01:25 +0300

    Remove unnecessary imports.

--- test/Utils/NumericTest.java
+++ test/Utils/NumericTest.java
@@ -7,9 +7,7 @@
 package Utils;
 
 import java.awt.Dimension;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
