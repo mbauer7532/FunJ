ad424d4820366a86ee4a2617892c88ba74f6aaba
Max Bauer <mbauer7532@gmail.com>
2013-11-25 06:45:11 +0200

    Remove unused import.

--- src/DataStructures/PersistentMapBase.java
+++ src/DataStructures/PersistentMapBase.java
@@ -12,7 +12,6 @@ import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
