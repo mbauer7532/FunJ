3bfeab10ec1642d32c64f077b1d7cfc39e03cc3b
Max Bauer <mbauer7532@gmail.com>
2013-12-04 16:28:38 +0200

    These tests have already been moved into the PersistentMap tests since they are universally applicable to other maps.

--- test/DataStructures/RedBlackTreeModuleTest.java
+++ test/DataStructures/RedBlackTreeModuleTest.java
@@ -6,20 +6,10 @@
 
 package DataStructures;
 
-import DataStructures.RedBlackTreeModule.Tree;
-import DataStructures.TuplesModule.Pair;
-import Utils.Numeric;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -39,26 +29,4 @@ public class RedBlackTreeModuleTest {
 
   @After
   public void tearDown() {}
-
-  private static <K extends Comparable<K>, V> void checkRedBlackTreeProperties(final Tree<K, V> t) {
-    final Pair<Boolean, String> res = RedBlackTreeModule.verifyRedBlackProperties(t);
-
-    if (! res.mx1) {
-      assertTrue(res.mx2, false);
-    }
-  }
-
-  /**
-   * Test of verifyRedBlackProperties method, of class RedBlackTreeModule.
-   */
-  @Test
-  public void testVerifyMapPropertiesForSimpleCases() {
-//    PersistentMapTest.verifyMapPropertiesForSimpleCases(sRedBlackTreeFactory);
-  }
-
-  @Test
-  public void testVerifyRedBlackPropertiesRandomSample() {
-//    PersistentMapTest.verifyMapPropertiesRandomSample(sRedBlackTreeFactory);
-  }
-
 }
