840388b01b6ce28e2fb368c70998d2c9b65f0aea
Max Bauer <mbauer7532@gmail.com>
2013-12-28 23:09:03 +0200

    Implement the usual toString(), equals(), and hashCode() methods for Refs.

--- src/Utils/Ref.java
+++ src/Utils/Ref.java
@@ -16,4 +16,26 @@ public class Ref<A> {
   public Ref(final A a) { r = a; }
 
   public A r;
+
+  @Override
+  public String toString() {
+    return String.format("Ref(%s)", r.toString());
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (obj instanceof Ref) {
+      @SuppressWarnings("unchecked")
+      final Ref<A> ref = (Ref<A>) obj;
+      return r.equals(ref.r);
+    }
+    else {
+      return false;
+    }
+  }
+
+  @Override
+  public int hashCode() {
+    return 47 + r.hashCode();
+  }
 }
