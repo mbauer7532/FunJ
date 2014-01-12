6a1c25538a10da7eaede81c3b8ba7141fb3f9990
Max Bauer <mbauer7532@gmail.com>
2014-01-12 17:29:38 +0200

    Javadoc.

--- src/DataStructures/PersistentHeap.java
+++ src/DataStructures/PersistentHeap.java
@@ -16,6 +16,7 @@ import org.StructureGraphic.v1.DSTreeNode;
  *
  * @author Neo
  * @param <V>
+ * @param <H>
  */
 public interface PersistentHeap<V extends Comparable<V>, H extends PersistentHeap<V, H>> extends Iterable<V>, DSTreeNode {
   public boolean isEmpty();
